7169f787f85b5c0419f28d82de18c64a50cefb49
Drak <drak@zikula.org>
2010-07-06 09:05:42 +0000

    Refactor for new API.

---
+++ src/Tour/lib/Tour/Version.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Zikula Application Framework
+ *
+ * @link http://www.zikula.org
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @author Simon Birtwistle simon@itbegins.co.uk
+ */
+
+class Tour_Version extends Zikula_Version
+{
+    public function getMetaData()
+    {
+        $meta = array();
+        $meta['displayname']      = __('Tour');
+        //!module name that appears in URL
+        $meta['url']              = __('tour');
+        $meta['description']      = __('First time configuration and Zikula Tour.');
+        $meta['version']          = '1.3';
+        $meta['contact']          = 'http://zikula.org/';
+        $meta['securityschema']   = array();
+        return $meta;
+    }
+}
