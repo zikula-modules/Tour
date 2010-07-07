6e1acb2e04f95f24f679336010893e572564f0e3
Drak <drak@zikula.org>
2010-07-07 08:14:41 +0000

    Refactor.

--- src/Tour/lib/Tour/Version.php
+++ src/Tour/lib/Tour/Version.php
@@ -16,7 +16,7 @@ class Tour_Version extends Zikula_Version
         //!module name that appears in URL
         $meta['url']              = __('tour');
         $meta['description']      = __('First time configuration and Zikula Tour.');
-        $meta['version']          = '1.3';
+        $meta['version']          = '1.3.0';
         $meta['contact']          = 'http://zikula.org/';
         $meta['securityschema']   = array();
         return $meta;
